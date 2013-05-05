18ce93527a1180ffb16be56bc9527168d9c35c9f
henghonglee <henghong.lee@gmail.com>
2013-05-06 00:55:14 +0800

    added github ouath

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,7 @@
 class HomeController < ApplicationController
   def index
-    #TODO: need to make this changable
-    if current_user
-      current_user.setGitHubUsername("henghonglee1")
-    end
+
   end
   
-  def setGitHubName
-      current_user.setGitHubUsername("")
 
-    redirect_to root_path
-  end
 end
