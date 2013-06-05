74fb3357330a0d19708c8ee3d487d6d452798593
henghonglee <henghong.lee@gmail.com>
2013-06-06 00:26:38 +0800

    added new repo hook

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Evernotedev::Application.routes.draw do
   resources :repo
   resources :devernote
   get '/auth/:provider/callback', to: 'sessions#create'
-  match '/repo_hook'. to:'repo#hook' ,:via => :post
+  match '/repo_hook' => 'repo#hook' ,:via => :post
   match '/set_github_username/:username' => 'devernote#refresh'
   match '/update_repo/:repo_id' => 'repo#update_repo'
   match '/remove_tag' => 'home#removetag'  
